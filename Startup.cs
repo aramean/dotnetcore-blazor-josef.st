ec4f8a620f1a91ef0d8a181ec87a7d6e3c5db68b
josef <Josef.Gabrielsson@goteborgsregionen.se>
2019-10-21 23:30:14 +0200

    Configure the middleware to forward the X-Forwarded-For and X-Forwarded-Proto headers

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using josef.Data;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace josef
 {
@@ -43,6 +44,12 @@ namespace josef
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
+
+                app.UseAuthentication();
             }
 
             app.UseHttpsRedirection();
