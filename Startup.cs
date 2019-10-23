cd1bd06d05835408cc8dffe557712434f99d261d
josef <Josef.Gabrielsson@goteborgsregionen.se>
2019-10-24 01:00:17 +0200

    Cleanup

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using josef.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using josef.Data;
-using Microsoft.AspNetCore.HttpOverrides;
 
 namespace josef
 {
