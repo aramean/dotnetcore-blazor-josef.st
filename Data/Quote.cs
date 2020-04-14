9460ba50679bcf6b7499e1a0bc61adf5251d7dcd
Josef Gabrielsson <Josef.Gabrielsson@goteborgsregionen.se>
2020-04-14 23:50:00 +0200

    My quotes

---
+++ Data/Quote.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace josef.Data
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Quotation { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
